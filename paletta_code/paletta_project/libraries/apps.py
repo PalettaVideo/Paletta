4df86a8d0513643ec22a4be32fa040009ceac2cf
dimitar-seraffimov <dimitar.seraffimov@gmail.com>
2025-06-21 11:22:35 +0100

    clean libraries/apps.py

--- paletta_code/paletta_project/libraries/apps.py
+++ paletta_code/paletta_project/libraries/apps.py
@@ -4,9 +4,3 @@ from django.apps import AppConfig
 class LibrariesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'libraries'
-
-    def ready(self):
-        """
-        Import signals when the app is ready.
-        """
-        import libraries.private_category
